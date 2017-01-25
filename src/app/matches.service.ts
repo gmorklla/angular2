92a231d2070a3e39e855419278da84f83fdcbfef
gmorklla <gmorklla@gmail.com>
2017-01-25 17:02:54 -0600

    Matches service reconfig

--- src/app/matches.service.ts
+++ src/app/matches.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { MdDialog } from '@angular/material';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Match } from './match';
 
 @Injectable()
@@ -8,8 +10,35 @@ export class MatchesService {
 		new Match('team3', 'team4', '15-02-17')
 	];
 
+	regMatches;
+	regPolls;
+	//match;
+
+	items: FirebaseListObservable<any[]>;
+
+	constructor(public af: AngularFire) {
+
+		/*this.match = af.database.list('/matches');
+
+		// Save data in Firebase list
+		this.match.push({
+			matches: this.matches
+		});*/
+
+		this.items = af.database.list('/matches');
+        // subscribe to changes
+        this.items.subscribe(queriedItems => {
+        	this.prepareMatchesList(queriedItems);        	
+        });
+	}
+
 	getMatches() {
 		return this.matches;
+	}
+
+	// Open a material dialog for feedback the user
+	prepareMatchesList(algo) {
+		console.log(algo[0].matches);
 	}
 
 }
